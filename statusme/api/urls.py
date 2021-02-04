ca3849b114d7fea3aac254708a6fc40f472cf688
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-02-04 19:42:06 +0600

    add serializer.py

--- statusme/api/urls.py
+++ statusme/api/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
-from .views import Index
+from .views import article_list
 
 urlpatterns = [
-    path('', Index),
+    path('articles/', article_list),
 ]
--- statusme/api/views.py
+++ statusme/api/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render,HttpResponse
-
+from .serializers import ArticleSerializer
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
 # Create your views here.
 
 
 
-def Index(request):
-    return HttpResponse("It is woking")
+def article_list(request):
+
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors,status=400)    
